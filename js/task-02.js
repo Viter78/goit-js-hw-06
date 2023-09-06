b82abd2c1a336bdecaf27e9992e2f7c698b9050f
Vitalii Terets <vitaliipool@gmail.com>
2023-09-06 23:31:05 +0200

    Update task-2.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsList = document.querySelector('#ingredients');
+
+ingredients.forEach(ingredient => {
+  const liEl = document.createElement('li');
+  liEl.textContent = ingredient;
+  liEl.classList.add('item');
+  ingredientsList.appendChild(liEl);
+});
